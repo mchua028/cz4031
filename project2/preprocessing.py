ee4d517e99e45df3ac0e3997f8fbdd91c1978186
shyamsundarko <shyamsundarko@gmail.com>
2022-11-03 23:18:45 +0800

    annotation partially done for each subquery

--- project2/preprocessing.py
+++ project2/preprocessing.py
@@ -4,11 +4,26 @@ import psycopg
 
 # Assume always binary tree
 class QueryPlanTreeNode:
-	def __init__(self, info: dict, left: Optional[Self] = None, right: Optional[Self] = None):
+	def __init__(self, info: dict, desc: Optional[str]="", left: Optional[Self] = None, right: Optional[Self] = None):
 		self.info = info
 		self.left = left
 		self.right = right
-	
+		self.desc = desc
+
+	def get_annotation(self) -> None: 
+		types=['Hash Cond', 'Cond'] #unsure if there are more types of filtering keys
+		for k in self.info.items():
+			if k[0] == "Node Type":
+				self.desc += "Perform "+k[1]
+			elif k[0] == "Relation Name":
+				self.desc += " on table "+(k[1].upper())
+			elif k[0] == "Alias":
+				self.desc += " as "+k[1]
+			elif k[0] in types:
+				self.desc += " and filtering on "+k[1]
+		
+		
+
 	def get_primary_info(self) -> dict:
 		# List of node types -> https://github.com/postgres/postgres/blob/master/src/backend/commands/explain.c#L1191
 		primary_info = {}
@@ -17,19 +32,37 @@ class QueryPlanTreeNode:
 				or k == "Alias" or k == "Group Key" or k == "Strategy" \
 				or ("Filter" in k and "Removed by" not in k) or "Cond" in k:
 				primary_info[k] = v
-		
+		self.get_annotation()
+		#print(self.desc)
 		return primary_info
-				
+
+
+	
+	
+	
+
 class QueryPlanTree:
 	root: Optional[QueryPlanTreeNode]
 
 	def __init__(self):
 		self.root = None
 	
+	def postOrder(root) -> str:
+		if root==None:
+			return ""
+        #traverse left subtree
+		QueryPlanTree.postOrder(root.left)
+        #traverse right subtree
+		QueryPlanTree.postOrder(root.right)
+        #traverse root
+		print(root.info)
+		print(root.desc)
+	
 	def __init__(self, cursor: psycopg.Cursor, query: str):
 		plan: dict = explain_query(cursor, query)["Plan"]
 		self.root = self._build(plan)
-
+		QueryPlanTree.postOrder(self.root)
+	
 	@staticmethod
 	def _build(plan: dict) -> Optional[QueryPlanTreeNode]:
 		if "Node Type" not in plan:
@@ -44,7 +77,7 @@ class QueryPlanTree:
 				cur.left = QueryPlanTree._build(subplans[0])
 			if len(subplans) >= 2:
 				cur.right = QueryPlanTree._build(subplans[1])
-
+		
 		return cur
 	
 	def __str__(self) -> str:
@@ -64,10 +97,12 @@ class QueryPlanTree:
 			right = "\n" + right 
 
 		node_type: str = node.info["Node Type"]
-		del node.info["Node Type"]
+		#del node.info["Node Type"]
 
 		return f"{'    ' * level}-> {node_type} {node.get_primary_info()}{left}{right}"
 
+	
+		
 def explain_query(cursor: psycopg.Cursor, query: str, debug: bool = False) -> dict:
 	cursor.execute(
 		f"EXPLAIN (FORMAT JSON, ANALYZE) {query}"
@@ -75,5 +110,5 @@ def explain_query(cursor: psycopg.Cursor, query: str, debug: bool = False) -> di
 	query_explanation = cursor.fetchone()[0][0]
 	if debug:
 		print("Query Explanation:", json.dumps(query_explanation, indent=2))
-
+	
 	return query_explanation
--- project2/project.py
+++ project2/project.py
@@ -3,7 +3,7 @@ import psycopg
 from interface import App
 
 if __name__ == "__main__":
-    conninfo = "postgresql://postgres:postgres@localhost:5432/tpchimported"
+    conninfo = "postgresql://postgres:Shyamvar1993@localhost:5432/tpchimported"
 
     with psycopg.connect(conninfo) as conn:
         with conn.cursor() as cursor:
