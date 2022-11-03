c60185c93434fb04c86ef57566b3333c6e502857
Chewposhi <chewposhi@gmail.com>
2022-11-04 00:32:22 +0800

    Update interface.py
    Added label, resized text window and increased button size

--- project2/interface.py
+++ project2/interface.py
@@ -17,16 +17,18 @@ class App(tk.Tk):
 
         self.ctx = Context(cursor)
 
+        self.topLabel = tk.Label(self, text="Enter SQL query below").grid(column=0, row=0)
+
         self.annotated_query_frame = AnnotatedQueryFrame(self, self.ctx)
-        self.annotated_query_frame.grid(column=0, row=1)
+        self.annotated_query_frame.grid(column=0, row=2)
         self.ctx.frames["annotated_query"] = self.annotated_query_frame
 
         self.visualize_query_plan_frame = VisualizeQueryPlanFrame(self, self.ctx)
-        self.visualize_query_plan_frame.grid(column=1, row=0, rowspan=2)
+        self.visualize_query_plan_frame.grid(column=1, row=1, rowspan=2)
         self.ctx.frames["visualize_query_plan"] = self.visualize_query_plan_frame
 
         self.input_query_frame = InputQueryFrame(self, self.ctx)
-        self.input_query_frame.grid(column=0, row=0)
+        self.input_query_frame.grid(column=0, row=1)
         self.ctx.frames["input_query"] = self.input_query_frame
 
 
@@ -52,10 +54,11 @@ class AnnotatedQueryFrame(ttk.Frame, Updatable):
         self.ctx = ctx
 
         self.annotated_query = ttk.Label(self, text="Annotation")
-        self.annotated_query.grid()
+        self.annotated_query.grid(column=0,row=0)
 
     def update_changes(self, *args, **kwargs):
         pass
+
 
 class VisualizeQueryPlanFrame(ttk.Frame, Updatable):
     def __init__(self, master: tk.Misc, ctx: Context):
@@ -74,10 +77,10 @@ class InputQueryFrame(ttk.Frame, Updatable):
     def __init__(self, master: tk.Misc, ctx: Context):
         super().__init__(master)
 
-        self.input_query = tk.Text(self)
+        self.input_query = tk.Text(self, height=10, width=50)
         self.input_query.grid(row=0)
 
-        self.analyze_query_button = ttk.Button(self, text="Analyze Query", command=self.analyze_query)
+        self.analyze_query_button = ttk.Button(self, text="Analyze Query", command=self.analyze_query, padding=10)
         self.analyze_query_button.grid(row=1)
 
         self.ctx = ctx
