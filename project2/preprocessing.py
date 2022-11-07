fa70823ec8ad7a2cc03f9f85002d6a0b3ff3d15f
railBitsAndHex <104562337+railBitsAndHex@users.noreply.github.com>
2022-11-07 14:19:36 +0800

    For checking of issue

--- project2/preprocessing.py
+++ project2/preprocessing.py
@@ -43,12 +43,16 @@ class QueryPlanTree:
 	def __init__(self):
 		self.root = None
 		self.scan_nodes = {}
+		self.join_nodes = {}
 
+	# get the query plan from the query
 	@staticmethod
 	def from_query(query: str, cursor: psycopg.Cursor):
 		plan = query_plan(query, cursor)
+		# returns a QueryPlanTree
 		return QueryPlanTree.from_plan(plan)
 
+	# Builds the QueryPlanTree
 	@staticmethod
 	def from_plan(plan: dict):
 		qptree = QueryPlanTree()
@@ -64,7 +68,7 @@ class QueryPlanTree:
 		cur = QueryPlanTreeNode(info)
 
 		# Track scan nodes
-		if "Scan" in plan["Node Type"]:
+		if "Scan" in plan["Node Type"] and plan['Node Type'] != "Bitmap Index Scan":
 			cur.involving_relations.add(
 				Relation(plan["Relation Name"], plan["Alias"])
 			)
@@ -112,10 +116,11 @@ def query_plan(query: str, cursor: psycopg.Cursor) -> dict:
 	cursor.execute(f"EXPLAIN (FORMAT JSON) {query}")
 	return cursor.fetchone()[0][0]["Plan"]
 
+# Generate the alternative Query plan
 def alternative_query_plans(query: str, cursor: psycopg.Cursor):
 	scan_types = ["bitmapscan", "indexscan", "indexonlyscan", "seqscan", "tidscan"]
 	join_types = ["hashjoin", "mergejoin", "nestloop"]
-
+	
 	for disabled_scan_types, disabled_join_types in product(
 		combinations(scan_types, len(scan_types) - 1),
 		combinations(join_types, len(join_types) - 1),
@@ -127,6 +132,28 @@ def alternative_query_plans(query: str, cursor: psycopg.Cursor):
 		yield query_plan(query, cursor)
 		cursor.connection.rollback()
 
+#  Generate the alternative q plan tree
+#  returns a generator object
 def alternative_query_plan_trees(query: str, cursor: psycopg.Cursor):
+	print(query)
 	for plan in alternative_query_plans(query, cursor):
 		yield QueryPlanTree.from_plan(plan)
+
+def traverse_aqp(node: Optional[QueryPlanTreeNode], join_nodes_dict: dict):
+	if node is None:
+		return ""
+	traverse_aqp(node.left, join_nodes_dict)
+	traverse_aqp(node.right, join_nodes_dict)
+	
+	node_info = node.info
+	for inv_relation in node.involving_relations:
+		print(inv_relation.relation_name)
+	print("\n")
+
+def handle_join_nodes(aqp_list: list[QueryPlanTree]) -> dict[str: dict]:
+	join_nodes_dict = dict()
+	for aqp in aqp_list: 
+		print("AQP")
+		print(aqp)
+		print("\nTraversing the aqp\n")
+		traverse_aqp(aqp.root, join_nodes_dict)
